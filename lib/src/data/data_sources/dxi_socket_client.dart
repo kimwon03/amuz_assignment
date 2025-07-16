a8b593a0226b4d238fc419dfb7dc7ed62f673bf1
kimwon03 <kimwon03@amuz.co.kr>
2025-07-16 15:31:17 +0900

    FEAT: 서버 인증 및 단방향 SecurityContext 발급

--- lib/src/data/data_sources/dxi_socket_client.dart
+++ lib/src/data/data_sources/dxi_socket_client.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:amuz_assignment/src/core/constants/dxi_constant.dart';
+import 'package:amuz_assignment/src/core/constants/keys.dart';
 import 'package:amuz_assignment/src/core/utils/base_socket_client.dart';
 
 class DxiSocketClient {
   final BaseSocketClient _socketClient = BaseSocketClient();
+
+  Future<void> serverAuthentication() async {
+    final SecurityContext securityContext = _getSecurityContext();
+  }
 
   Future<void> connect() async {
     _socketClient.disconnect();
@@ -10,5 +19,15 @@ class DxiSocketClient {
     bool socketConnectResult = await _socketClient.connect(host, port);
 
     if (!socketConnectResult) false;
+  }
+
+  SecurityContext _getSecurityContext() {
+    final SecurityContext securityContext = SecurityContext.defaultContext;
+
+    final Uint8List privateKeyBytes = base64Decode(Keys.blackboxKey);
+
+    securityContext.usePrivateKeyBytes(privateKeyBytes);
+
+    return securityContext;
   }
 }
