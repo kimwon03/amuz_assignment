c2552d2d8e685293fff5cb3b3325b3390d54463e
kimwon03 <kimwon03@amuz.co.kr>
2025-07-16 11:18:02 +0900

    FEAT: Socket connet 기능 추가

--- lib/src/core/utils/base_socket_client.dart
+++ lib/src/core/utils/base_socket_client.dart
@@ -3,7 +3,13 @@ import 'dart:io';
 class BaseSocketClient {
   Socket? socket;
 
-  Future<bool> connet(String ip, int port) {
-    throw UnimplementedError();
+  Future<bool> connet(String ip, int port) async {
+    try {
+      socket = await Socket.connect(ip, port, timeout: Duration(seconds: 2));
+
+      return true;
+    } catch(e) {
+      return false;
+    }
   }
 }
