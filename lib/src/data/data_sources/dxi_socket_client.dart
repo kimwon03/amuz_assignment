deee179d1658994ddf562e227684cd3cd848f030
kimwon03 <kimwon03@amuz.co.kr>
2025-07-16 13:37:11 +0900

    FEAT: Dxi 연결 함수 기본 틀 생성

--- lib/src/data/data_sources/dxi_socket_client.dart
+++ lib/src/data/data_sources/dxi_socket_client.dart
@@ -2,4 +2,8 @@ import 'package:amuz_assignment/src/core/utils/base_socket_client.dart';
 
 class DxiSocketClient {
   final BaseSocketClient _socketClient = BaseSocketClient();
+
+  Future<void> connect() {
+    throw UnimplementedError();
+  }
 }
