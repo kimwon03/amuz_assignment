a074126658001b9edb54ee33ce1ed528e637b515
kimwon03 <kimwon03@amuz.co.kr>
2025-07-16 13:57:19 +0900

    FEAT: Dxi 소켓 연결

--- lib/src/data/data_sources/dxi_socket_client.dart
+++ lib/src/data/data_sources/dxi_socket_client.dart
@@ -1,3 +1,4 @@
+import 'package:amuz_assignment/src/core/constants/dxi_constant.dart';
 import 'package:amuz_assignment/src/core/utils/base_socket_client.dart';
 
 class DxiSocketClient {
@@ -5,5 +6,9 @@ class DxiSocketClient {
 
   Future<void> connect() async {
     _socketClient.disconnect();
+
+    bool socketConnectResult = await _socketClient.connect(host, port);
+
+    if (!socketConnectResult) false;
   }
 }
