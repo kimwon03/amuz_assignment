2e00ca97196d29cac9d0c46ee386dfe8949729f0
kimwon03 <kimwon03@amuz.co.kr>
2025-07-17 13:37:34 +0900

    FEAT: ConnectButton Widget 생성

---
+++ lib/src/presentation/pages/connect_page/local_widgets/connect_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ConnectButton extends StatelessWidget {
+  const ConnectButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: null, child: Text('연결'));
+  }
+}
