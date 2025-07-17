0ec5a211230d8e32b3a07f48af36d4c7d7d4c59c
kimwon03 <kimwon03@amuz.co.kr>
2025-07-17 12:27:34 +0900

    FEAT: Connect Page 생성

--- lib/src/core/routes/app_pages.dart
+++ lib/src/core/routes/app_pages.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 final class AppPages {
   const AppPages._();
 
-  static final String _initialLocation = '/';
+  static final String _initialLocation = AppRoutes.connect.path;
 
   static GoRouter routeConfig = GoRouter(
     routes: AppRoutes.routes,
--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -1,7 +1,14 @@
+import 'package:amuz_assignment/src/presentation/pages/connect_page/connect_page.dart';
 import 'package:go_router/go_router.dart';
 
 final class AppRoutes {
   const AppRoutes._();
 
-  static List<RouteBase> routes = [];
+  static GoRoute connect = GoRoute(
+    path: '/connect',
+    name: 'connect',
+    builder: (context, state) => ConnectPage(),
+  );
+
+  static List<RouteBase> routes = [connect];
 }
