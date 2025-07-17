59d580a3a631633625d97158f0742b614cc371d9
kimwon03 <kimwon03@amuz.co.kr>
2025-07-17 13:54:54 +0900

    FEAT: DxiRepository 생성

---
+++ lib/src/data/repositories/dxi_repository_impl.dart
@@ -0,0 +1,8 @@
+import 'package:amuz_assignment/src/data/data_sources/dxi_socket_client.dart';
+import 'package:amuz_assignment/src/domain/repositories/dxi_repository.dart';
+
+class DxiRepositoryImpl implements DxiRepository {
+  late final DxiSocketClient _client;
+
+  DxiRepositoryImpl({required DxiSocketClient client}) : _client = client;
+}
