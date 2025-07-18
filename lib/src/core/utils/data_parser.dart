a0356506c49392199c06e24e1667beb10f8e15fd
kimwon03 <kimwon03@amuz.co.kr>
2025-07-18 17:39:29 +0900

    FEAT: CRC Bit 생성하는 함수 추가

--- lib/src/core/utils/data_parser.dart
+++ lib/src/core/utils/data_parser.dart
@@ -1,7 +1,20 @@
 import 'package:convert/convert.dart';
 
 List<int> toHexList(int value, [int size = 2]) {
-    String hexStr = value.toRadixString(16).padLeft(2, '0').padRight(size * 2, '0');
+  String hexStr = value
+      .toRadixString(16)
+      .padLeft(2, '0')
+      .padRight(size * 2, '0');
 
   return hex.decode(hexStr);
+}
+
+int generateCrc8Bit(List<int> data) {
+  int sum = 0;
+
+  for (final int value in data) {
+    sum += value;
+  }
+
+  return (sum ^ 0x55) % 256;
 }
