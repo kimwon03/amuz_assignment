e54a51b7cbf29e91d2a70efc69342a18e12ee86d
kimwon03 <kimwon03@amuz.co.kr>
2025-07-18 13:31:11 +0900

    FEAT: 전달 받은 ByteData 출력

--- lib/src/data/data_sources/dxi_socket_client.dart
+++ lib/src/data/data_sources/dxi_socket_client.dart
@@ -153,6 +153,7 @@ class DxiSocketClient {
         _whenReceviedPing();
         break;
       case Cmd.sendDxiData:
+        appLog.d('Get Byte ${result['data']['bytes']}');
         break;
       case Cmd.setDxiMode:
         Map<String, dynamic> data = result['data'];
