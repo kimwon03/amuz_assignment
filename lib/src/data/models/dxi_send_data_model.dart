73aa3c2d3f705ea59a2c40ac955e0ae1cf2e7cad
kimwon03 <kimwon03@amuz.co.kr>
2025-07-18 20:02:51 +0900

    FEAT: DxiSendDataModel 생성

---
+++ lib/src/data/models/dxi_send_data_model.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'dxi_send_data_model.freezed.dart';
+part 'dxi_send_data_model.g.dart';
+
+@Freezed(toJson: true)
+abstract class DxiSendDataModel with _$DxiSendDataModel {
+  const factory DxiSendDataModel({
+    String? bytes,
+    @Default('Y') String constantConnect,
+  }) = _DxiSendDataModel;
+}
