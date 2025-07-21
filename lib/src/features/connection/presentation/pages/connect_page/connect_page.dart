f6fe3314ec23b6ef8be72f34e4bc93aaeae2da73
kimwon03 <kimwon03@amuz.co.kr>
2025-07-21 16:54:00 +0900

    FEAT: 연결 성공 시 mainPage로 이동

--- lib/src/features/connection/presentation/pages/connect_page/connect_page.dart
+++ lib/src/features/connection/presentation/pages/connect_page/connect_page.dart
@@ -1,30 +1,35 @@
-import 'package:amuz_assignment/src/core/common/models/connect_state.dart' as Utils;
+import 'package:amuz_assignment/src/core/common/models/connect_state.dart'
+    as Utils;
+import 'package:amuz_assignment/src/core/routes/app_routes.dart';
 import 'package:amuz_assignment/src/features/connection/presentation/notifier/connect_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'local_widgets/index.dart';
 
-class ConnectPage extends StatelessWidget {
+class ConnectPage extends ConsumerWidget {
   const ConnectPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final Utils.ConnectionState state = ref.watch(connectNotifierProvider);
+
+    if (state == Utils.ConnectionState.connect) {
+      Future.delayed(Duration.zero, () {
+        ref.read(connectNotifierProvider.notifier).dispose();
+
+        if (context.mounted) context.go(AppRoutes.main.path);
+      });
+    }
+
     return Scaffold(
       body: Center(
-        child: Consumer(
-          builder: (_, ref, ___) {
-            Utils.ConnectionState state = ref.watch(
-              connectNotifierProvider,
-            );
-
-            return switch (state) {
-              Utils.ConnectionState.connect => DisconnectButton(),
-              Utils.ConnectionState.waiting => Text('연결 중'),
-              Utils.ConnectionState.disconnect => ConnectButton(),
-            };
-          },
-        ),
+        child: switch (state) {
+          Utils.ConnectionState.connect => DisconnectButton(),
+          Utils.ConnectionState.waiting => Text('연결 중'),
+          Utils.ConnectionState.disconnect => ConnectButton(),
+        },
       ),
     );
   }
