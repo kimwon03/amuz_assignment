8255a57c6735ec1783333cdf7785b2277ebf1953
kimwon03 <kimwon03@amuz.co.kr>
2025-07-22 18:38:50 +0900

    FEAT: MainPage가 켜졌을 경우 startMonitoring 시작

--- lib/src/features/monitoring/presentation/pages/main_page/main_page.dart
+++ lib/src/features/monitoring/presentation/pages/main_page/main_page.dart
@@ -1,10 +1,17 @@
+import 'package:amuz_assignment/src/features/monitoring/presentation/notifier/main_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends HookConsumerWidget {
   const MainPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      ref.read(mainNotifierProvider.notifier).startMonitoring();
+    }, []);
+
     return Scaffold(body: Center(child: Text('Main Page')));
   }
 }
