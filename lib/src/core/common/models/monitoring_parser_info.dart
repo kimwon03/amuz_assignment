118877d09b7b9a0024caa42419b31d7076022b74
kimwon03 <kimwon03@amuz.co.kr>
2025-07-22 14:51:36 +0900

    FEAT: MonitoringParserInfo 파라미터 변경

--- lib/src/core/common/models/monitoring_parser_info.dart
+++ lib/src/core/common/models/monitoring_parser_info.dart
@@ -1,13 +1,18 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'monitoring_parser_info.freezed.dart';
+part 'monitoring_parser_info.g.dart';
 
-@freezed
+@Freezed(fromJson: true)
 abstract class MonitoringParserInfo with _$MonitoringParserInfo {
   const factory MonitoringParserInfo({
     required String name,
-    required String type,
-    required int length,
-    required bool signed,
+    required String length,
+    required bool control,
+    @Default('int') @JsonKey(includeIfNull: false) String type,
+    required String desc,
+    @Default(false) @JsonKey(includeIfNull: false) bool? signed,
+    @JsonKey(includeIfNull: false) String? deco,
+    @JsonKey(includeIfNull: false) Map<String, dynamic>? map,
   }) = _MonitoringParserInfo;
 }
