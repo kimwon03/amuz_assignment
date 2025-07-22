3f98adca606f2165b9acaf80f877f1179c729b41
kimwon03 <kimwon03@amuz.co.kr>
2025-07-22 19:27:11 +0900

    FEAT: DataListView를 ConsumerWidget으로 변경 및 MainNotifier.state를 watch함

--- lib/src/features/monitoring/presentation/pages/main_page/local_widgets/data_listview.dart
+++ lib/src/features/monitoring/presentation/pages/main_page/local_widgets/data_listview.dart
@@ -1,10 +1,14 @@
+import 'package:amuz_assignment/src/features/monitoring/presentation/notifier/main_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class DataListview extends StatelessWidget {
+class DataListview extends ConsumerWidget {
   const DataListview({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final Map<String, dynamic> dataMap = ref.watch(mainNotifierProvider);
+
+    return ExpansionPanelList(children: []);
   }
 }
